13a324c8a2ad2ad758142a684a2edee9153a114c
KIGMAN <Valentine Kiguli>
2024-08-14 21:04:55 +0300

    Made migrations

--- demo_files/demo_app/models.py
+++ demo_files/demo_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Simple Database model
+class TodoItem(models.Model): 
+    title = models.CharField(max_length=200) 
+    completed = models.BooleanField(default=False)
